68483b49cb8d70d1a7797438164c7cd89d6b4829
pallavigusain92 <47799976+pallavigusain92@users.noreply.github.com>
2019-02-28 11:15:33 -0800

    Delete file from remote server - Pallavi's story
    Delete

--- _commands/_delete.py
+++ _commands/_delete.py
@@ -4,11 +4,11 @@ class Command:
         Command to perform delete command on the connected FTP host.
 
         Args:
-            filepath - path of the file to be deleted from the remote server.
+            filepath - Path of the file to be deleted from the remote server.
         """
 		
 		
 		try: 
 			response = self._perform_ftp_command('remove', filepath)
 		except IOError as e:
-			print("File doesn't exists")
+			print("File doesn't exist")
