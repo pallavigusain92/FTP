fe6a6560e15a60579da8d5383fd6c4b33c9e8c48
pallavigusain92 <47799976+pallavigusain92@users.noreply.github.com>
2019-02-28 11:08:35 -0800

    Get multiple - Pallavi's story
    Get a file from remote server

--- _commands/_getmultiple.py
+++ _commands/_getmultiple.py
@@ -1,10 +1,10 @@
 class Command:
 	def do_getmultiple(self,remotepath):
 		"""
-        Command to perform get files command from the remote server.
+        Command to perform 'get multiple' command from the remote server.
 
         Args:
-            filepath - path of the file to be deleted from the remote server.
+            remotepath - both Source and Destination.
         """
 		file_path = remotepath.split(" ")
 		if len(remote_src_dest)!=2:
